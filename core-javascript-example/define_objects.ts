b487d3da1fba07053cc5011d729bba52ea008b35
Tushar Khairnar <tushar.khairnar@hotmail.com>
2026-09-12 13:49:56 +0530

    Restructure

--- core-javascript-example/define_objects.ts
+++ core-javascript-example/define_objects.ts
@@ -1,3 +1,5 @@
+// Different ways to define objects in TypeScript
+
 const obj1 = {
     name: "Object 1",
     value: 42,
@@ -15,11 +17,6 @@ const obj3 = {
     value: 168,
 };
 
-const obj4 = Object.create({});
-obj4.name = "Object 4";
-obj4.value = 336;
-obj4.isActive = false;
-
 class Obj5 {
     name: string | undefined;
     value: number | undefined;
@@ -31,6 +28,12 @@ obj5.name = "Object 5";
 obj5.value = 672;
 obj5.isActive = true;
 
-const obj.pro
+const obj4 = Object.create({});
+obj4.name = "Object 4";
+obj4.value = 336;
+obj4.isActive = false;
 
-
+const obj6 = Object.create(obj5);
+obj6.name = "Object 6";
+obj6.value = 1344;
+obj6.isActive = false;
